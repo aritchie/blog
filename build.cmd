94d25eb12738bd930c2354a44bbf6c42685f5a8b
Allan Ritchie <allan.ritchie@gmail.com>
2019-02-09 14:19:01 -0500

    updates

---
+++ build.cmd
@@ -0,0 +1,3 @@
+rem dotnet tool install --gloabl wyam.tool
+wyam build
+pause
---
+++ preview.cmd
@@ -0,0 +1,3 @@
+rem dotnet tool install --g Wyam.Tool
+"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5080
+wyam preview
