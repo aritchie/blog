0d5740dc6043aa0ee7931d691c6e77fb47747c73
Allan Ritchie <allan.ritchie@gmail.com>
2026-04-03 10:42:25 -0400

    Tag cloud

--- src/content.config.ts
+++ src/content.config.ts
@@ -24,6 +24,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
